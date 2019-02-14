2b6c2636e3436043ef9ed536c29a9938f7142866
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-02-14 12:52:43 -0500

    Added admin screen to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Admin from './components/Admin';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container-fluid">
+        <h1>Service</h1>
+        <Router>
+          <div>
+            <Link to="/admin">Admin</Link>
+            <Route
+                path="/admin"
+                exact
+                component={Admin}/>
+          </div>
+        </Router>
       </div>
     );
   }
