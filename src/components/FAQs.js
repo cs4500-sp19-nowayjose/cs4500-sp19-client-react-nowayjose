48898896435db82aa36d774f85ed10f46039e727
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-02-14 12:36:56 -0500

    Created initial FAQs screen

---
+++ src/components/FAQs.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import FAQService from '../services/FAQService'
+class FAQs extends React.Component {
+    constructor(props) {
+        super(props)
+        this.faqService = FAQService.getInstance()
+        this.state = {
+            faqs: []
+        }
+    }
+    componentDidMount() {
+        this.faqService
+            .findAllFAQs()
+            .then(faqs =>
+                this.setState({
+                    faqs: faqs
+                })
+            )
+    }
+    render() {
+        
+    }
+}
+
+export default FAQs
