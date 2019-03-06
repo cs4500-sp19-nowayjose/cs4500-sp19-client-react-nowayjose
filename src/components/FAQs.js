cb0fdb7909f63449f7740f346958db3d1db09ff1
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-03-06 17:54:40 -0500

    Added bindings and table for edit part of table

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -5,8 +5,19 @@ class FAQs extends React.Component {
         super(props)
         this.faqService = FAQService.getInstance()
         this.state = {
-            faqs: []
+            faqs: [],
+            title: 'New title',
+            question: 'New question',
+            updateId: -1
         }
+
+        this.editFAQ = this.editFAQ.bind(this)
+        this.deleteFAQ = this.deleteFAQ.bind(this)
+        this.createFAQ = this.createFAQ.bind(this)
+        this.updateFAQ = this.updateFAQ.bind(this)
+        this.handleTitleChange = this.handleTitleChange.bind(this)
+        this.handleQuestionChange = this.handleQuestionChange.bind(this)
+
     }
     componentDidMount() {
         this.faqService
@@ -17,21 +28,38 @@ class FAQs extends React.Component {
                 })
             )
     }
+
+
     render() {
-        return(
+        return (
             <div>
                 <h3>Frequently Asked Questions:</h3>
                 <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Title</th>
+                            <th scope="col">Question</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                        <tr>
+                            <th> <input type="text" onChange={this.handleTitleChange} value={this.state.title} placeholder="title" /> </th>
+                            <th> <input type="text" onChange={this.handleQuestionChange} value={this.state.question} placeholder="question" /> </th>
+                            <th> <button type="button" onClick={this.updateFAQ} class="btn btn-primary btn-block">Save</button> </th>
+                            <th> <button type="button" onClick={this.createFAQ} class="btn btn-primary btn-block">Create</button> </th>
+                        </tr>
+                    </thead>
                     <tbody>
-                    {
-                        this.state.faqs
-                            .map(faq =>
-                                <tr key={faq.id}>
-                                    <td>{faq.title}</td>
-                                    <td>{faq.question}</td>
-                                </tr>
-                            )
-                    }
+                        {
+                            this.state.faqs
+                                .map(faq =>
+                                    <tr key={faq.id}>
+                                        <td>{faq.title}</td>
+                                        <td>{faq.question}</td>
+                                        <th> <button type="button" onClick={() => this.editFAQ(faq)} class="btn btn-primary btn-block">Edit</button>  </th>
+                                        <th> <button type="button" onClick={() => this.deleteFAQ(faq)} class="btn btn-primary btn-block">Delete</button>    </th>
+                                    </tr>
+                                )
+                        }
                     </tbody>
                 </table>
             </div>
