7b2dffc9d19faccf9ae24ab09a8189833f4a1d67
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-03-06 17:29:18 -0500

    Added calls to create and delete

--- src/services/FAQService.js
+++ src/services/FAQService.js
@@ -13,4 +13,24 @@ export default class FAQService {
     findAllFAQs = () =>
         fetch("https://cs4500-sp19-nowayjose.herokuapp.com/api/faqs")
             .then(response => response.json())
+
+    createFAQ(faq) {
+        return fetch('https://cs4500-sp19-nowayjose.herokuapp.com/api/faqs', {
+        method: 'post',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(faq)
+        })
+        .then(response => response.json())
+    }
+
+    deleteFAQ(faq) {
+        return fetch('https://cs4500-sp19-nowayjose.herokuapp.com/api/faq/' + faq.id, {
+            method: 'delete',
+            headers: {
+                'content-type': 'application/json'
+            }
+            });
+    }
 }
