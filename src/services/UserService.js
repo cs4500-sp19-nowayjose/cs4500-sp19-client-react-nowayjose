1223138c75caf8c4d438b2b1dcc6f0e2315c892e
ysmolyar <yonismolyar1@gmail.com>
2019-03-14 18:01:06 -0400

    add CRUD functions to user service

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,3 +1,7 @@
+
+const USER_API_URL = 'https://cs4500-sp19-nowayjose.herokuapp.com/api/users';
+const API = 'https://cs4500-sp19-nowayjose.herokuapp.com/api/';
+
 export default class UserService {
     static instance = null;
     static getInstance() {
@@ -7,9 +11,40 @@ export default class UserService {
         return this.instance
     }
     findUserById = userId =>
-        fetch(`https://cs4500-sp19-nowayjose.herokuapp.com/api/${userId}`)
+        fetch(USER_API_URL + `/${userId}`)
             .then(response => response.json())
     findAllUsers = () =>
-        fetch("https://cs4500-sp19-nowayjose.herokuapp.com/api/users")
+        fetch(USER_API_URL)
             .then(response => response.json())
+
+    createUser = (user) => {
+        fetch(USER_API_URL, {
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            method: 'POST'
+        }).then(response => {
+            return response.json();
+        })
+    }
+
+    updateUser = (userId, user) => {
+        fetch(API + `${userId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+    }
+
+    deleteUser = (userId) => {
+        fetch(USER_API_URL + `${userId}`, {
+            method: 'DELETE'
+            }
+        ).then(response => {
+                return response;
+            });
+    }
 }
