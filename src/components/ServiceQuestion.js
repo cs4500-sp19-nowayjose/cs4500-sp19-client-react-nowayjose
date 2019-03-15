023ea666c87d1b02ef36cc51c5e51931ad783626
Adam Tobey <adamtobey@gmail.com>
2019-03-15 12:48:11 -0400

    Add pagination to service questions

--- src/components/ServiceQuestion.js
+++ src/components/ServiceQuestion.js
@@ -5,16 +5,32 @@ class ServiceQuestions extends React.Component {
         super(props)
         this.serviceQuestionService = ServiceQuestionService.getInstance()
         this.state = {
-            serviceQuestions: []
+            serviceQuestions: [],
+            page: 0,
+            resultsPerPage: 10
         }
+    }
+    setPage(pageNumber) {
+      this.setState(Object.assign(this.state, {
+        page: pageNumber
+      }))
+    }
+    setResultsPerPage(value) {
+      var resultsPerPage = this.state.serviceQuestions.length
+      if (value !== "all") {
+        resultsPerPage = parseInt(value)
+      }
+      this.setState(Object.assign(this.state, {
+        resultsPerPage: resultsPerPage
+      }))
     }
     componentDidMount() {
         this.serviceQuestionService
             .findAllServiceQuestions()
             .then(serviceQuestions =>
-                this.setState({
+                this.setState(Object.assign(this.state, {
                     serviceQuestions: serviceQuestions
-                })
+                }))
             )
     }
     render() {
@@ -25,6 +41,7 @@ class ServiceQuestions extends React.Component {
                     <tbody>
                     {
                         this.state.serviceQuestions
+                            .slice(this.state.page * this.state.resultsPerPage, (this.state.page + 1) * this.state.resultsPerPage)
                             .map(serviceQuestion =>
                                 <tr key={serviceQuestion.id}>
                                     <td>
@@ -35,6 +52,20 @@ class ServiceQuestions extends React.Component {
                     }
                     </tbody>
                 </table>
+                <div>
+                  {
+                    [...Array(Math.ceil(this.state.serviceQuestions.length / this.state.resultsPerPage)).keys()].map(pageNumber =>
+                      <a onClick={() => this.setPage(pageNumber)}>{pageNumber + 1} </a>
+                    )
+                  }
+                  <select onChange={(e) => this.setResultsPerPage(e.target.value)}>
+                    <option value="10">10</option>
+                    <option value="25">25</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                    <option value="all">all</option>
+                  </select>
+                </div>
             </div>
         )
     }
