7973cab2f9bbfbb8d8bd4d8c11d5abd969502a3f
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-03-22 13:24:17 -0400

    Added find faqs

---
+++ src/components/FAQs.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import FAQs from './FAQs';
+import renderer from 'react-test-renderer';
+
+
+test('Link changes the class when hovered', () =>
+{   const component = renderer.create(
+       <FAQs> </FAQs>)})
