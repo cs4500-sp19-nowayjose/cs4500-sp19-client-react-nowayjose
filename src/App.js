c765e61fd4d10b599084ac6fed8448086736104b
Adam Tobey <adamtobey@gmail.com>
2019-03-22 16:11:53 -0400

    Refactor answers component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Admin from './components/Admin';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// FIXME import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
