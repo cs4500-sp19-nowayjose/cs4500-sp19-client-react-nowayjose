f07e4d1966e6ef875d9cf3d8f12cecead2ebabb4
Adam Tobey <adamtobey@gmail.com>
2019-03-22 17:15:15 -0400

    Mock fetch all and delete

--- src/services/ServiceAnswer.mock.js
+++ src/services/ServiceAnswer.mock.js
@@ -10,8 +10,6 @@ function setUp() {
       switch (method.toUpperCase()) {
         case "DELETE":
           return handleDelete(url)
-        case "POST":
-          return handlePost(url, body)
         default:
           return { "default": "default" }
       }
@@ -20,14 +18,8 @@ function setUp() {
 
 function handleGet(url) {
   const returnData = {};
-  // mock findServiceAnswerById
-  if (url.search(/\/api\/service_question_answers\/\d+/) !== -1) {
-    const questionId = parseInt(url.slice(url.search(/\/\d+\b/) + 1));
-    returnData.json = function() {
-      return serviceAnswerMockData.filter(({ id }) => id === questionId)[0]
-    }
   // mock findAllServiceAnswers
-} else if (url.indexOf('api/service_question_answers') !== -1) {
+  if (url.indexOf('api/service_question_answers') !== -1) {
     returnData.json = function() {
       return serviceAnswerMockData
     }
@@ -38,20 +30,6 @@ function handleGet(url) {
 function handleDelete(url) {
   // delete is a void function
   return new Promise(resolve => resolve());
-}
-
-function handlePost(url, body) {
-  const returnData = {};
-  const { id } = JSON.parse(body);
-  // mock findServiceAnswerById
-  if (url.indexOf('api/service_question/filter') !== -1) {
-    returnData.json = function() {
-      return serviceAnswerMockData.filter(q => {
-        return q.id === id;
-      })
-    }
-  }
-  return new Promise(resolve => resolve(returnData));
 }
 
 export default setUp;
--- src/services/ServiceAnswer.test.js
+++ src/services/ServiceAnswer.test.js
@@ -6,16 +6,6 @@ beforeAll(function() {
   setUp()
 })
 
-test('fetch by id', function() {
-  return Service.findServiceAnswerById(1)
-    .then(response => {
-      expect(response).toBeDefined()
-      expect(response.id).toBe(1)
-      expect(response.minRangeAnswer).toBe(600)
-      expect(response.maxRangeAnswer).toBe(25000)
-    })
-})
-
 test('fetch all questions', function() {
   return Service.findAllServiceAnswers()
     .then(response => {
