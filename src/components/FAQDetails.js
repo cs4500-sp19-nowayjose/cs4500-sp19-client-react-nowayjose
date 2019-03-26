38cbdf54a3f0e946733a9456fdba3ed5cb5c74f6
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-03-26 17:23:35 -0400

    Removed all the dependencies from the component

--- src/components/FAQDetails.js
+++ src/components/FAQDetails.js
@@ -1,73 +1,31 @@
 import React from 'react'
-import FAQService from '../services/FAQService'
-class FAQDetails extends React.Component {
-    constructor(props) {
-        super(props)
-        this.faqService = FAQService.getInstance()
-        this.state = {
-            faqs: [],
-            faq: {
-                choiceAnswer: '',
-                id: 1
+const FAQDetails = ({ faqs, faq, selectFAQ }) =>
+    <div>
+        <h3>FAQ Details</h3>
+        <select
+            value={faq.id}
+            onChange={(e) => selectFAQ(e.target.value)}
+            className="form-control">
+            {
+                faqs
+                    .map(faq =>
+                        <option
+                            value={faq.id}
+                            key={faq.id}>
+                            {faq.id}
+                        </option>
+                    )
             }
-        }
-    }
-    componentDidMount() {
-        this.faqService
-            .findAllFAQs()
-            .then(faqs => {
-                    this.faqService.findFAQById(this.props.match.params.id).then(faq => {
-                        this.setState({
-                            faqs: faqs,
-                            faq: faq
-                        })
-                    })
-                }
-            )
-    }
-
-    selectFAQ = id =>
-        this.faqService
-            .findFAQById(id)
-            .then(faq => {
-                    this.props.history.push("/admin/faqs/" + id)
-                    this.setState({
-                        faq: faq
-                    })
-                }
-            )
-    render() {
-        return(
-            <div>
-                <h3>FAQ Details</h3>
-                <select
-                    value={this.state.faq.id}
-                    onChange={(e) => this.selectFAQ(e.target.value)}
-                    className="form-control">
-                    {
-                        this.state.faqs
-                            .map(faq =>
-                                <option
-                                    value={faq.id}
-                                    key={faq.id}>
-                                    {faq.id}
-                                </option>
-                            )
-                    }
-                </select>
-                <br/>
-                <input
-                    onChange={() => {}}
-                    className="form-control"
-                    value={this.state.faq.title}/>
-                <br/>
-                <input
-                    onChange={() => {}}
-                    className="form-control"
-                    value={this.state.faq.question}/>
-            </div>
-        )
-    }
-}
-
+        </select>
+        <br />
+        <input
+            onChange={() => { }}
+            className="form-control"
+            value={faq.title} />
+        <br />
+        <input
+            onChange={() => { }}
+            className="form-control"
+            value={faq.question} />
+    </div>
 export default FAQDetails
