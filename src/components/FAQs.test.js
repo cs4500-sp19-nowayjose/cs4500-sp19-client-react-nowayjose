67f1e0df3a21839a89ddc01c9b05ef20d8a6d9e6
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-03-26 16:21:40 -0400

    Removed component test

--- src/components/FAQs.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import FAQs from './FAQs';
-import renderer from 'react-test-renderer';
-
-
-test('Link changes the class when hovered', () =>
-{   const component = renderer.create(
-       <FAQs> </FAQs>)})
