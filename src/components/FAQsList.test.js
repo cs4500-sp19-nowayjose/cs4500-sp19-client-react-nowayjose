f148ce2ff2154684b0dc40c554a8318c2d49e721
Jack Lawhorn <lawhorn.j@husky.neu.edu>
2019-03-26 19:30:58 -0400

    FAQsList DOM test

--- src/components/FAQsList.test.js
+++ src/components/FAQsList.test.js
@@ -1,33 +1,81 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import FAQsList from './FAQsList';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import FAQsList from './FAQsList'
+import renderer from 'react-test-renderer'
+import faqs from '../../data/frequentlyAskedQuestion.mock.json'
+import {BrowserRouter as Router} from 'react-router-dom'
 
-test('renders faq correctly', function() {
-  const component = renderer.create(
-    <FAQsList
-      faqs={[{"answers": [],"id": 2,"question": "Favorite color?","title": "Second question"}]}
-      title=""
-      question=""
-      updateId=-1
-      recordsNumber=10
-      page=1
-      filter={{}}
+let container;
 
-      editFAQ = {() => {}}
-      deleteFAQ = {() => {}}
-      createFAQ = {() => {}}
-      updateFAQ = {() => {}}
-      getPageNumbers = {() => {return [1]}}
-      passesFilter = {() => {return true}}
-      selectFAQ = {() => {}}
-      handleTitleChange = {() => {}}
-      handleQuestionChange = {() => {}}
-      handlePageChange = {() => {}}
-      handleRecordsNumberChange = {() => {}}
-      handleFilterChange = {() => {}}
-    />
-  )
-  const testInstance = component.root;
-  expect(testInstance.findByProps({ className: "faq-row"})).toBeDefined();
-  expect(testInstance.findByProps({ className: "faq-row"})).lengthToBe(1);
-}) 
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+})
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+})
+
+it('has the correct number of results', () => {
+  act(() => {
+    ReactDOM.render(<Router>
+      <FAQsList
+        faqs={faqs}
+        title=""
+        question=""
+        updateId=-1
+        recordsNumber=10
+        page=1
+        filter={{}}
+
+        editFAQ = {() => {}}
+        deleteFAQ = {() => {}}
+        createFAQ = {() => {}}
+        updateFAQ = {() => {}}
+        getPageNumbers = {() => {return [1]}}
+        passesFilter = {() => {return true}}
+        selectFAQ = {() => {}}
+        handleTitleChange = {() => {}}
+        handleQuestionChange = {() => {}}
+        handlePageChange = {() => {}}
+        handleRecordsNumberChange = {() => {}}
+        handleFilterChange = {() => {}}
+      />
+    </Router>, container)
+  });
+  var rows = container.getElementsByClassName("faq-row");
+  expect(rows.length).toBe(3);
+})
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <Router>
+      <FAQsList
+        faqs={faqs}
+        title=""
+        question=""
+        updateId=-1
+        recordsNumber=10
+        page=1
+        filter={{}}
+
+        editFAQ = {() => {}}
+        deleteFAQ = {() => {}}
+        createFAQ = {() => {}}
+        updateFAQ = {() => {}}
+        getPageNumbers = {() => {return [1]}}
+        passesFilter = {() => {return true}}
+        selectFAQ = {() => {}}
+        handleTitleChange = {() => {}}
+        handleQuestionChange = {() => {}}
+        handlePageChange = {() => {}}
+        handleRecordsNumberChange = {() => {}}
+        handleFilterChange = {() => {}}
+      />
+      </Router>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+})
