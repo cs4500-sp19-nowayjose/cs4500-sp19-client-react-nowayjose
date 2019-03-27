c05a9f8e0ced031d52498d87e85ba5962e165f27
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-03-26 20:25:05 -0400

    Added on change test

--- src/components/FAQDetails.test.js
+++ src/components/FAQDetails.test.js
@@ -23,4 +23,24 @@ test('render faq details correctly', function() {
     expect(testInstance.findByProps({ className: "question form-control" })).toBeDefined()
   })
 
-  
+  test('render faq details correctly after changing question', function() {
+    const component = renderer.create(
+      <FAQDetails
+        selectFAQ={() => {}}
+        faq={{  
+            "answers": [],
+            "id": 6,
+            "question": "Did you pass a background check?",
+            "title": "First question"
+        }}
+        faqs={frequentlyAskedQuestionData}
+
+      />
+    )
+    const testInstance = component.root;
+    let   selectFaq    = testInstance.findByProps({className: 'selectedFaq'})
+    selectFaq.props.onChange({target: {value: '4'}})
+    expect(testInstance.findByProps({ className: "selectedFaq"})).toBeDefined();
+    expect(testInstance.findByProps({ className: "title form-control" })).toBeDefined();
+    expect(testInstance.findByProps({ className: "question form-control" })).toBeDefined()
+  })
