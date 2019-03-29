5de96645e2d5b92386febeeb6bec49a39293db9c
Kathan Master <kathan.master@yahoo.com>
2019-03-29 04:43:10 -0400

    Created base faq answers container

---
+++ src/components/FaqAnswersContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import FAQAnswers from './FaqAnswers'
+import FAQAnswerService from '../services/FAQAnswerService'
+
+class FAQAnswersContainer extends React.Component {
+    
+}
