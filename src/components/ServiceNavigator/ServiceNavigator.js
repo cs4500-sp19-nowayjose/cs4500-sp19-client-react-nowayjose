6fa5f33e2017d330230ae8de1f3e03c94e56fa44
Kathan Master <kathan.master@yahoo.com>
2019-04-01 23:46:57 -0400

    removed uncessary search bar

--- src/components/ServiceNavigator/ServiceNavigator.js
+++ src/components/ServiceNavigator/ServiceNavigator.js
@@ -2,22 +2,9 @@ import React from 'react'
 import ServiceCategoryList from './ServiceCategoryList'
 import ServiceCategorySectionList from './ServiceCategorySectionList'
 import serviceCategories from '../../data/service-categories.mock.json'
-import SearchBar from '../SearchBar/SearchBar'
 
 const ServiceNavigator = () =>
     <div>
-        <div className="row">
-            <div className="col-8">
-                <SearchBar/>
-            </div>
-            <div className="col-3 text-right">
-                <a href="#">Sign up</a>
-            </div>
-            <div className="col-1">
-                <a href="#">Log in</a>
-            </div>
-        </div>
-        <br/>
         <br/>
         <div className="row">
             <div className="col-3">
