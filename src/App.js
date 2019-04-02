f5a54f42d60b4bbc7db9973eae2c8021b3a02506
Benjamin Clauss <benjamin.h.clauss@gmail.com>
2019-04-02 19:21:07 -0400

    Add home screen

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
 import React, { Component } from 'react';
-import './App.css';
-import Admin from './components/Admin';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
-import ServiceProviderSearch from './components/ServiceProviderSearch'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import ServiceNavigatorComponent from './containers/ServiceNavigatorContainer';
+import '../node_modules/font-awesome/css/font-awesome.min.css'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Admin from './components/Admin'
+import Home from './components/Home'
+// import ServiceNavigator from './components/ServiceNavigator/ServiceNavigator'
+// import ServiceProviderNavigator from './components/ServiceProviderNavigator/ServiceProviderNavigator'
 
 class App extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <h1>Service</h1>
-        <Router>
-          <div>
+        <div className="container">
+          <Router>
             <div>
-              <Link to="/admin">Admin</Link>
+              <Link to="/home">Home</Link> |
+              <Link to="/services"> Services</Link> |
+              <Link to="/providers"> Providers</Link> |
+              <Link to="/admin"> Admin</Link> |
+              <Link to="/provider"> Provider</Link>
+              <br/>
+              <br/>
+              <br/>
+
+              <Route
+                  path="/home"
+                  exact
+                  component={Home}/>
+              {/*<Route*/}
+                  {/*path="/services"*/}
+                  {/*exact*/}
+                  {/*component={ServiceNavigator}/>*/}
               <Route
                   path="/admin"
                   exact
                   component={Admin}/>
+              {/*<Route*/}
+                  {/*path="/providers"*/}
+                  {/*exact*/}
+                  {/*component={ServiceProviderNavigator}/>*/}
             </div>
-            <div>
-              <Link to="/provider-search">Provider search</Link>
-              <Route
-                  path="/provider-search"
-                  exact
-                  component={ServiceProviderSearch}/>
-            </div>
-            <div>
-              <Link to="/service-categories">service categories</Link>
-              <Route
-                  path="/service-categories"
-                  exact
-                  component={ServiceNavigatorComponent}/>
-            </div>
-            </div>
-        </Router>
-      </div>
+          </Router>
+        </div>
     );
   }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+// import SearchBar from './SearchBar/SearchBar'
+// import ServiceTabNavigator from './ServiceTabNavigator/ServiceTabNavigator'
+// import ServiceCategoryPills from './ServiceCategoryPills/ServiceCategoryPills'
+// import serviceCategories from '../data/service-categories.mock.json'
+const Home = ({history}) =>
+    <div>
+        <div className="row">
+            <div className="col-8">
+                <h1>
+                    Find professionals near you.
+                </h1>
+                {/*<SearchBar history={history}/>*/}
+            </div>
+        </div>
+
+        <br/>
+        <br/>
+        <br/>
+        <div>
+            {/*<ServiceCategoryPills/>*/}
+        </div>
+        <br/>
+        <br/>
+        <br/>
+        {/*<ServiceTabNavigator*/}
+            {/*serviceCategories={serviceCategories}/>*/}
+    </div>
+
+export default Home
