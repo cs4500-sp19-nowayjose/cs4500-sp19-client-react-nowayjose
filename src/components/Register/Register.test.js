feb88d8633e54be89b1f11ea2487b237776cf4d8
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-04-08 18:05:15 -0400

    Test for rendering labels

--- src/components/Register/Register.test.js
+++ src/components/Register/Register.test.js
@@ -14,3 +14,18 @@ test('render create button correctly', function () {
     expect(testInstance.findByProps({ className: "btn btn-primary btn-block" })).toBeDefined();
 })
 
+test('render labels correctly', function () {
+    const component = renderer.create(
+        <Register
+            handleCreate={(e) => { }}
+            handleChange={(e) => { }}
+        />
+    )
+    const testInstance = component.root;
+    expect(testInstance.findByProps({ className: "first-name form-control" })).toBeDefined();
+    expect(testInstance.findByProps({ className: "last-name form-control" })).toBeDefined();
+    expect(testInstance.findByProps({ className: "email form-control" })).toBeDefined();
+    expect(testInstance.findByProps({ className: "password form-control" })).toBeDefined();
+
+})
+
