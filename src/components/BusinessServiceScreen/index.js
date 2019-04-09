2b4e6239827241b98bc5cc99dff9fea7632aab93
Calvin Zhao <MmZzyh@Calvins-MacBook.local>
2019-04-09 14:54:45 -0400

    Update answers

--- src/components/BusinessServiceScreen/index.js
+++ src/components/BusinessServiceScreen/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import ServiceSelectSidebar from './serviceSelectSidebar'
 import ServiceQuestionsForm from './serviceQuestionsForm'
 import ServiceQuestionService from '../../services/ServiceQuestionService'
-import serviceAnswerService from '../../services/ServiceAnswerService'
+import ServiceAnswerService from '../../services/ServiceAnswerService'
 
 class BusinessServiceScreen extends React.Component {
   serviceQuestionService = ServiceQuestionService.getInstance()
-  serviceAnswersService = ServiceQuestionService.getInstance()
+  serviceAnswersService = ServiceAnswerService.getInstance()
+  providerId = this.props.match.params.providerId
   state = {
     selectedServices: [],
     possibleServices: [],
@@ -58,6 +59,34 @@ class BusinessServiceScreen extends React.Component {
     }))
   }
 
+  submitAnswers() {
+    let questions = (this.state.selectedServiceQuestions[this.state.activeServiceId] || [])
+    let requests = questions.map(question => {
+      let answer = this.state.serviceQuestionAnswers[question.id]
+      let newAnswerBody = {
+        "trueFalseAnswer": null,
+        "minRangeAnswer": null,
+        "maxRangeAnswer": null,
+        "choiceAnswer": null,
+        "serviceQuestion": {"id": question.id},
+        "serviceProvider": {"id": this.providerId}
+      }
+      if (question.serviceQuestionType === "MINMAX") {
+        newAnswerBody["minRangeAnswer"] = answer["min"]
+        newAnswerBody["maxRangeAnswer"] = answer["max"]
+      } else if (question.serviceQuestionType === "YESORNO") {
+        newAnswerBody["trueFalseAnswer"] = answer
+      } else if (question.serviceQuestionType === "MULTIPLECHOICES"){
+        newAnswerBody["choiceAnswer"] = answer
+      } else {
+        console.log("Weird", question, answer)
+      }
+      console.log("create answer", newAnswerBody)
+      return this.serviceAnswersService.create(newAnswerBody)
+    })
+    return Promise.all(requests)
+  }
+
   render() {
     return (
       <div className="row">
@@ -75,7 +104,7 @@ class BusinessServiceScreen extends React.Component {
         </div>
         <div className="col-9">
           <ServiceQuestionsForm
-            submitAnswers={() => console.log(this.state.serviceQuestionAnswers)}
+            submitAnswers={this.submitAnswers.bind(this)}
             serviceQuestions={this.state.selectedServiceQuestions[this.state.activeServiceId] || []}
             answers={this.state.serviceQuestionAnswers}
             answerQuestion={this.setAnswer.bind(this)} />
