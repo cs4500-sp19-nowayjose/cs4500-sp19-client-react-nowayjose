3fe9da6154a0f0003486207d9035c5a239588c5b
Benjamin Clauss <benjamin.h.clauss@gmail.com>
2019-04-09 10:49:36 -0400

    Refactor home screen

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/font-awesome/css/font-awesome.min.css'
 import './App.css';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
-import Admin from './components/Admin'
-import Home from './components/Home'
-import ServiceProviderSearch from './components/ServiceProviderSearch'
-// import ServiceNavigator from './components/ServiceNavigator/ServiceNavigator'
-import ServiceProviderNavigator from './components/ServiceProviderSearch'
-import ServiceNavigatorContainer from './containers/ServiceNavigatorContainer'
-import serviceCategories from './data/service-categories.mock.json'
 import ServiceCategoryService from './services/ServiceCategoryService'
-import Register from './components/Register/Register';
+import serviceCategories from './data/service-categories.mock.json'
+import Home from './components/Home'
+import ServiceNavigatorContainer from './containers/ServiceNavigatorContainer'
+import ServiceProviderNavigator from './components/ServiceProviderSearch'
+import Admin from './components/Admin'
 import RegisterContainer from './containers/RegisterContainer';
 
 class App extends Component {
@@ -29,59 +26,52 @@ class App extends Component {
             }))
     }
 
-  render() {
-    return (
-        <div className="container">
-          <Router>
-            <div>
-              <div className="d-flex justify-content-between">
-                <div>
-                  <Link to="/home">Home</Link> |
-                  <Link to="/services"> Services</Link> |
-                  <Link to="/providers/123"> Providers</Link> |
-                  <Link to="/admin"> Admin</Link>
-                  <Link to="/provider"> Provider</Link>
-                </div>
-                <div className="d-flex flex-row-reverse">
-                  <Link to="/register"> Register</Link>
-                  <div style={{ marginLeft: 10, marginRight: 10 }}/>
-                  <Link to="#"> Log in</Link>
-                </div>
-              </div>
-              <br/>
-              <br/>
-              <br/>
+    render() {
+        return (
+            <div className="container">
+                <Router>
+                    <div>
+                        <div className="d-flex justify-content-between">
+                            <div>
+                                <Link to="/home">Home</Link> |
+                                <Link to="/services"> Services</Link> |
+                                <Link to="/providers"> Providers</Link> |
+                                <Link to="/admin"> Admin</Link> |
+                                <Link to="/provider/2"> Provider</Link> |
+                                <Link to="/register"> Register</Link> |
+                                <Link to="#"> Log in</Link>
+                            </div>
+                        </div>
+                        <br/>
+                        <br/>
+                        <br/>
 
-              <Route
-                  path="/home"
-                  exact
-                  render={() => <Home pillServiceCategories={this.state.pillServiceCategories}/>}/>
+                        <Route
+                            path="/home"
+                            exact
+                            render={() => <Home pillServiceCategories={this.state.pillServiceCategories}/>}/>
+                        <Route
+                            path="/services"
+                            exact
+                            component={ServiceNavigatorContainer}/>
+                        <Route
+                            path="/providers"
+                            exact
+                            component={ServiceProviderNavigator}/>
+                        <Route
+                            path="/admin"
+                            exact
+                            component={Admin}/>
 
-              <Route
-                  path="/providers/:id"
-                  exact
-                  component={ServiceProviderNavigator}/>
-              <Route
-                  path="/admin"
-                  exact
-                  component={Admin}/>
-              <Route
-                  path="/services"
-                  exact
-                  component={ServiceNavigatorContainer}/>
-              <Route
-                  path="/register"
-                  exact
-                  component={RegisterContainer}/>
-              <Route
-                  path="/provider-search/:id"
-                  exact
-                  component={ServiceProviderSearch}/>
+                        <Route
+                            path="/register"
+                            exact
+                            component={RegisterContainer}/>
+                    </div>
+                </Router>
             </div>
-          </Router>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
