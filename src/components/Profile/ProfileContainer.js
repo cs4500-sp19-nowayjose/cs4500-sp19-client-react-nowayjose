09fd3088e49ac5cfc75a303383e845e862c40df9
Jack Lawhorn <lawhorn.j@husky.neu.edu>
2019-04-09 11:27:28 -0400

    Implement Profile Screen

---
+++ src/components/Profile/ProfileContainer.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import UserService from '../../services/UserService'
+import ProfileForm from './ProfileForm'
+
+class ProfileContainer extends React.Component {
+     constructor(props) {
+        super(props)
+        this.userService = UserService.getInstance()
+        this.state = {
+            user: null,
+            firstName: "",
+            lastName: "",
+            dobMonth: 0,
+            dobDay: 0,
+            dobYear: 0,
+            addStreet: "",
+            addCity: "",
+            addState: "",
+            addZip: 0,
+            email: "",
+        }
+       
+        this.handleChange = this.handleChange.bind(this)
+        this.handleUpdate = this.handleUpdate.bind(this)
+       
+    }
+    
+    componentDidMount() {
+        this.userService
+            .findUserById(367)
+            .then(currentUser =>
+                this.setState({
+                    user: currentUser,
+                    firstName: currentUser.firstName,
+                    lastName: currentUser.lastName,
+                    dobMonth: currentUser.dobMonth,
+                    dobDay: currentUser.dobDay,
+                    dobYear: currentUser.dobYear,
+                    addStreet: currentUser.addStreet,
+                    addCity: currentUser.addCity,
+                    addState: currentUser.addState,
+                    addZip: currentUser.addZip,
+                    email: currentUser.email,
+                })
+            )
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+    }
+  
+    handleUpdate(event) {
+        var newUser = {
+            "id": this.state.user.id,
+            "username": this.state.user.username,
+            "email": this.state.user.email,
+            "firstName": this.state.firstName,
+            "lastName": this.state.lastName,
+            "dobMonth": this.state.dobMonth,
+            "dobDay": this.state.dobDay,
+            "dobYear": this.state.dobYear,
+            "addStreet": this.state.addStreet,
+            "addCity": this.state.addCity,
+            "addState": this.state.addState,
+            "addZip": this.state.addZip,
+            "email": this.state.email,
+        };
+      
+        this.userService.updateUser(newUser).then(resp => {
+            if (resp.status == 403) {
+                alert('Error: Unable to update user profile')
+            }
+            else {
+                this.props.history.push('/home')
+            }
+        });
+    }
+
+    render () {
+        return(
+            <ProfileForm
+                user={this.state.user}
+                firstName = {this.state.firstName}
+                lastName = {this.state.lastName}
+                dobMonth = {this.state.dobMonth}
+                dobDay = {this.state.dobDay}
+                dobYear = {this.state.dobYear}
+                addStreet = {this.state.addStreet}
+                addCity = {this.state.addCity}
+                addState = {this.state.addState}
+                addZip = {this.state.addZip}
+                email = {this.state.email}
+          
+                handleChange = {this.handleChange}
+                handleUpdate = {this.handleUpdate}
+            />
+        )
+    }
+}
+
+export default ProfileContainer
