f1d237d914d1088a1f3e3aa50df595b8c28d19cd
Jack Lawhorn <lawhorn.j@husky.neu.edu>
2019-04-12 16:45:03 -0400

    Fixed getProfile from current session

--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -28,25 +28,25 @@ class ProfileContainer extends React.Component {
     
     componentDidMount() {
         this.userService
-            .findAllUsers()
-            .then(users => {
+            .getProfile()
+            .then(user => {
                     this.setState({
-                        user: users[0],
-                        firstName: users[0].firstName,
-                        lastName: users[0].lastName,
+                        user: user,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
                       
                         // Setting DOB fields using lowercase versions
                         // Still trying to figure out why this is an issue
                         // These fields don't update
-                        dobMonth: users[0].dobmonth,
-                        dobDay: users[0].dobday,
-                        dobYear: users[0].dobyear,
+                        dobMonth: user.dobmonth,
+                        dobDay: user.dobday,
+                        dobYear: user.dobyear,
                         
-                        addStreet: users[0].addStreet,
-                        addCity: users[0].addCity,
-                        addState: users[0].addState,
-                        addZip: users[0].addZip,
-                        username: users[0].username,
+                        addStreet: user.addStreet,
+                        addCity: user.addCity,
+                        addState: user.addState,
+                        addZip: user.addZip,
+                        username: user.username,
                     });
                 }
             )
@@ -63,7 +63,6 @@ class ProfileContainer extends React.Component {
       
         var newUser = {
             "id": this.state.user.id,
-            "username": this.state.user.username,
             "username": this.state.user.username,
             "firstName": this.state.firstName,
             "lastName": this.state.lastName,
