f69b1ff128676a106080d9a0ffc8b857c13f832e
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-04-22 23:25:03 -0400

    Added test for creating question

--- src/components/FAQDetails.test.js
+++ src/components/FAQDetails.test.js
@@ -44,3 +44,24 @@ test('render faq details correctly', function() {
     expect(testInstance.findByProps({ className: "title form-control" })).toBeDefined();
     expect(testInstance.findByProps({ className: "question form-control" })).toBeDefined()
   })
+
+  test('render faq details correctly redirects to FAQs after creating question', function() {
+    const component = renderer.create(
+      <FAQDetails
+        selectFAQ={() => {}}
+        faq={{  
+            "answers": [],
+            "id": 6,
+            "question": "Did you pass a background check?",
+            "title": "First question"
+        }}
+        faqs={frequentlyAskedQuestionData}
+
+      />
+    )
+    const testInstance = component.root;
+    let   createFAQ    = testInstance.findByProps({className: 'create'})
+    createFAQ.props.onChange()
+    expect(testInstance.findByProps({ className: "faq-row"})).toBeDefined();
+  })
+
