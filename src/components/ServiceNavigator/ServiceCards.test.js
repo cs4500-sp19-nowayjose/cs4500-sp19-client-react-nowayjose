a91dd3877b6f4dfe305ac1c9c410b44659e60a12
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-04-22 20:46:02 -0400

    Added more data to service categories mock

--- src/components/ServiceNavigator/ServiceCards.test.js
+++ src/components/ServiceNavigator/ServiceCards.test.js
@@ -24,13 +24,7 @@ it('loads all cards w/ images', () => {
       <ServiceCards services={serviceCategories[0].services.slice(0, 4)} />,
     container);
   });
-  expect(container.querySelector('.card').length).toBe(4);
-  expect(container.querySelector('.card-img-top').length).toBe(4);
-  act(() => {
-    ReactDOM.render(
-      <ServiceCards services={serviceCategories[0].services.slice(0, 3)} />,
-    container);
-  });
-  expect(container.querySelector('.card').length).toBe(3);
-  expect(container.querySelector('.card-img-top').length).toBe(3);
+
+    expect(container.querySelector('.card-img-top').length).toBe(4);
+    expect(container.querySelector('.card').length).toBe(4);
 })
