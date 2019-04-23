a07f918d3d61c5259e1b96a15a8df0e7ae282f3d
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-04-22 23:25:41 -0400

    Added test for updating question

--- src/components/FAQDetails.test.js
+++ src/components/FAQDetails.test.js
@@ -84,3 +84,25 @@ test('render faq details correctly', function() {
     deleteFAQ.props.onChange()
     expect(testInstance.findByProps({ className: "faq-row"})).toBeDefined();
   })
+
+  test('render faq details correctly redirects to FAQS after updating question', function() {
+    const component = renderer.create(
+      <FAQDetails
+        selectFAQ={() => {}}
+        faq={{  
+            "answers": [],
+            "id": 6,
+            "question": "Did you pass a background check?",
+            "title": "First question"
+        }}
+        faqs={frequentlyAskedQuestionData}
+
+      />
+    )
+    const testInstance = component.root;
+    let   updateFAQ    = testInstance.findByProps({className: 'update'})
+    updateFAQ.props.onChange()
+    expect(testInstance.findByProps({ className: "selectedFaq"})).toBeDefined();
+    expect(testInstance.findByProps({ className: "title form-control" })).toBeDefined();
+    expect(testInstance.findByProps({ className: "question form-control" })).toBeDefined()
+  })
