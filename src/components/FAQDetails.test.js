31b11b89752caaf0c8f17fc29a93a7641f52be7b
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-04-22 23:25:24 -0400

    Added test for deleting question

--- src/components/FAQDetails.test.js
+++ src/components/FAQDetails.test.js
@@ -65,3 +65,22 @@ test('render faq details correctly', function() {
     expect(testInstance.findByProps({ className: "faq-row"})).toBeDefined();
   })
 
+  test('render faq details correctly redirects to FAQS after deleting question', function() {
+    const component = renderer.create(
+      <FAQDetails
+        selectFAQ={() => {}}
+        faq={{  
+            "answers": [],
+            "id": 6,
+            "question": "Did you pass a background check?",
+            "title": "First question"
+        }}
+        faqs={frequentlyAskedQuestionData}
+
+      />
+    )
+    const testInstance = component.root;
+    let   deleteFAQ    = testInstance.findByProps({className: 'delete'})
+    deleteFAQ.props.onChange()
+    expect(testInstance.findByProps({ className: "faq-row"})).toBeDefined();
+  })
