da4711a01f12500d57bd25c17c505c71165b0c66
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-04-22 23:26:18 -0400

    Added cancel test

--- src/components/FAQDetails.test.js
+++ src/components/FAQDetails.test.js
@@ -85,6 +85,26 @@ test('render faq details correctly', function() {
     expect(testInstance.findByProps({ className: "faq-row"})).toBeDefined();
   })
 
+  test('render faq details correctly redirects to FAQS after cancel', function() {
+    const component = renderer.create(
+      <FAQDetails
+        selectFAQ={() => {}}
+        faq={{  
+            "answers": [],
+            "id": 6,
+            "question": "Did you pass a background check?",
+            "title": "First question"
+        }}
+        faqs={frequentlyAskedQuestionData}
+
+      />
+    )
+    const testInstance = component.root;
+    let   cancel    = testInstance.findByProps({className: 'cancel'})
+    cancel.props.onChange()
+    expect(testInstance.findByProps({ className: "faq-row"})).toBeDefined();
+  })
+
   test('render faq details correctly redirects to FAQS after updating question', function() {
     const component = renderer.create(
       <FAQDetails
