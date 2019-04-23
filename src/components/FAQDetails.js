f2acc36489ebb3bc4fd6978d7c470b336072dc0a
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-04-22 22:06:25 -0400

    Changed the passed in components

--- src/components/FAQDetails.js
+++ src/components/FAQDetails.js
@@ -1,31 +1,39 @@
 import React from 'react'
-const FAQDetails = ({ faqs, faq, selectFAQ }) =>
+const FAQDetails = ({ faqs, faq, selectFAQ, create, update, remove, handleTitleChange, handleQuestionChange}) =>
     <div>
-        <h3>FAQ Details</h3>
-        <select
-            value={faq.id}
-            onChange={(e) => selectFAQ(e.target.value)}
-            className="selectedFaq" >
-            {
-                faqs
-                    .map(faq =>
-                        <option
-                            value={faq.id}
-                            key={faq.id}>
-                            {faq.id}
-                        </option>
-                    )
-            }
-        </select>
-        <br />
-        <input
-            onChange={() => { }}
-            className="title form-control"
-            value={faq.title} />
-        <br />
-        <input
-            onChange={() => { }}
-            className="question form-control"
-            value={faq.question} />
+        <div>
+            <h3>FAQ Details</h3>
+            <select
+                value={faq.id}
+                onChange={(e) => selectFAQ(e.target.value)}
+                className="selectedFaq" >
+                {
+                    faqs
+                        .map(faq =>
+                            <option
+                                value={faq.id}
+                                key={faq.id}>
+                                {faq.id}
+                            </option>
+                        )
+                }
+            </select>
+            <br />
+            <input type="text" onChange={handleTitleChange} value={faq.title} placeholder={faq.title}/>
+            <br />
+            <input type="text" onChange={handleQuestionChange} value={faq.question} placeholder={faq.question} />
+        </div>
+
+        <div>
+            <button onClick={(e) => create()}>
+                Create
+                </button>
+            <button onClick={(e) => remove()}>
+                Delete
+                </button>
+            <button onClick={(e) => update()}>
+                Update
+                </button>
+        </div>
     </div>
 export default FAQDetails
