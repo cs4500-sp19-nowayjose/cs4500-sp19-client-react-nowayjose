d0dc3bde0307889323ab8a12330203e0baec7e45
Adam Tobey <adamtobey@gmail.com>
2019-04-22 21:58:44 -0400

    Snapshot test

--- src/components/BusinessServiceScreen/serviceSelectSidebar.test.js
+++ src/components/BusinessServiceScreen/serviceSelectSidebar.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
 import { act } from 'react-dom/test-utils'
 
 import ServiceSelectSidebar from './serviceSelectSidebar';
@@ -41,4 +42,23 @@ test('it selects a service', function () {
     });
     document.body.querySelector(".select-service-button").dispatchEvent(new MouseEvent('click', {bubbles: true}));
     expect(called).toBe(true);
+})
+
+test('it renders properly', function() {
+  const tree = renderer
+    .create(
+        <ServiceSelectSidebar
+          query="a"
+          setQuery={() => {}}
+          possibleServices={services}
+          setPossibleServices={() => {}}
+          activeServiceId={0}
+          addService={() => {}}
+          removeService={() => {}}
+          selectService={() => {called = true;}}
+          selectedServices={services}
+          />
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 })
